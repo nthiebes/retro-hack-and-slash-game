09147ecc0f6a17c37b93baa0b7dc14aa0be3d644
Nico Thiebes <nico.thiebes@xing.com>
2024-01-29 13:08:26 +0100

    update biomes config

--- server/config.js
+++ server/config.js
@@ -2,26 +2,12 @@ const config = {
   chunkSize: 30,
   biomeNeighbours: {
     plain: ['plain', 'lightForest', 'flowerPlain', 'rockyPlain', 'savannah'],
-    flowerPlain: ['flowerPlain', 'plain', 'lightForest', 'rockyPlain'],
-    rockyPlain: ['flowerPlain', 'plain', 'lightForest', 'rockyPlain'],
-    lightForest: [
-      'plain',
-      'flowerPlain',
-      'rockyPlain',
-      'lightForest',
-      'forest',
-      'blackForest',
-      'deadForest'
-    ],
-    forest: ['plain', 'forest', 'lightForest', 'blackForest', 'deadForest'],
-    blackForest: [
-      'plain',
-      'blackForest',
-      'lightForest',
-      'forest',
-      'deadForest'
-    ],
-    deadForest: ['plain', 'deadForest', 'blackForest', 'lightForest', 'forest'],
+    flowerPlain: ['flowerPlain', 'plain', 'lightForest'],
+    rockyPlain: ['plain', 'rockyPlain', 'lightForest'],
+    lightForest: ['plain', 'forest'],
+    forest: ['forest', 'lightForest', 'blackForest', 'deadForest'],
+    blackForest: ['blackForest', 'forest'],
+    deadForest: ['deadForest', 'forest'],
     desert: ['desert', 'savannah', 'oasis'],
     oasis: ['desert', 'oasis'],
     savannah: ['savannah', 'desert', 'plain']
